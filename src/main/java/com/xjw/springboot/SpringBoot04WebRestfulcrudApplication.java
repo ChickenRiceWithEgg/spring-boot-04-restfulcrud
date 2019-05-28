79f597a787221da7507b270d8e0a969fd6a8268b
ChickenRiceWithEgg <jingwei144@foxmail.com>
2019-05-29 00:32:56 +0800

    springboot与web开发  自己配置一个视图

--- src/main/java/com/xjw/springboot/SpringBoot04WebRestfulcrudApplication.java
+++ src/main/java/com/xjw/springboot/SpringBoot04WebRestfulcrudApplication.java
@@ -2,12 +2,31 @@ package com.xjw.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;    //视图解析器
 
-@SpringBootApplication
+import java.util.Locale;
+
+@SpringBootApplication   //这个组合注解的作用：将这个主配置类所在的包下面的所有组件都扫描到Spring容器中
 public class SpringBoot04WebRestfulcrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
     }
+
+    @Bean
+    public ViewResolver myViewResolver(){
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver{
+
+        @Override
+        public View resolveViewName(String viewName, Locale locale) throws Exception {
+            return null;
+        }
+    }
+
 
 }
